050bd2a003dbbe4c85769fdf6ad5883506774905
Yuri Pourre <yuriamayuri@gmail.com>
2014-12-17 22:39:09 -0200

    Update code

--- rust_cam/rust_cam.c
+++ rust_cam/rust_cam.c
@@ -1,74 +1,217 @@
-/* based on code posted at: http://stackoverflow.com/questions/15031983/usb-webcam-image-capture-in-c-without-opencv-linux */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-
-#include <getopt.h>             /* getopt_long() */
-
-#include <fcntl.h>              /* low-level i/o */
-#include <unistd.h>
 #include <errno.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/ioctl.h>
-
+#include <fcntl.h>
 #include <linux/videodev2.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-#define CLEAR(x) memset (&(x), 0, sizeof (x))
-
-static int xioctl(int fh, int request, void *arg)
+uint8_t *buffer;
+ 
+static int xioctl(int fd, int request, void *arg)
 {
-    int r;
-    do {
-        r = ioctl(fh, request, arg);
-    } while (-1 == r && EINTR == errno);
-    return r;
+        int r;
+ 
+        do r = ioctl (fd, request, arg);
+        while (-1 == r && EINTR == errno);
+ 
+        return r;
+}
+ 
+int print_caps(int fd)
+{
+        struct v4l2_capability caps = {};
+        if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &caps))
+        {
+                perror("Querying Capabilities");
+                return 1;
+        }
+ 
+        printf( "Driver Caps:\n"
+                "  Driver: \"%s\"\n"
+                "  Card: \"%s\"\n"
+                "  Bus: \"%s\"\n"
+                "  Version: %d.%d\n"
+                "  Capabilities: %08x\n",
+                caps.driver,
+                caps.card,
+                caps.bus_info,
+                (caps.version>>16)&&0xff,
+                (caps.version>>24)&&0xff,
+                caps.capabilities);
+ 
+ 
+        struct v4l2_cropcap cropcap = {0};
+        cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        if (-1 == xioctl (fd, VIDIOC_CROPCAP, &cropcap))
+        {
+                perror("Querying Cropping Capabilities");
+                return 1;
+        }
+ 
+        printf( "Camera Cropping:\n"
+                "  Bounds: %dx%d+%d+%d\n"
+                "  Default: %dx%d+%d+%d\n"
+                "  Aspect: %d/%d\n",
+                cropcap.bounds.width, cropcap.bounds.height, cropcap.bounds.left, cropcap.bounds.top,
+                cropcap.defrect.width, cropcap.defrect.height, cropcap.defrect.left, cropcap.defrect.top,
+                cropcap.pixelaspect.numerator, cropcap.pixelaspect.denominator);
+ 
+        int support_grbg10 = 0;
+ 
+        struct v4l2_fmtdesc fmtdesc = {0};
+        fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        char fourcc[5] = {0};
+        char c, e;
+        printf("  FMT : CE Desc\n--------------------\n");
+        while (0 == xioctl(fd, VIDIOC_ENUM_FMT, &fmtdesc))
+        {
+                strncpy(fourcc, (char *)&fmtdesc.pixelformat, 4);
+                if (fmtdesc.pixelformat == V4L2_PIX_FMT_SGRBG10)
+                    support_grbg10 = 1;
+                c = fmtdesc.flags & 1? 'C' : ' ';
+                e = fmtdesc.flags & 2? 'E' : ' ';
+                printf("  %s: %c%c %s\n", fourcc, c, e, fmtdesc.description);
+                fmtdesc.index++;
+        }
+        /*
+        if (!support_grbg10)
+        {
+            printf("Doesn't support GRBG10.\n");
+            return 1;
+        }*/
+ 
+        struct v4l2_format fmt = {0};
+        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        fmt.fmt.pix.width = 640;
+        fmt.fmt.pix.height = 480;
+        //fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_BGR24;
+        //fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_GREY;
+        //fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
+        fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
+        fmt.fmt.pix.field = V4L2_FIELD_NONE;
+        
+        if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
+        {
+            perror("Setting Pixel Format");
+            return 1;
+        }
+ 
+        strncpy(fourcc, (char *)&fmt.fmt.pix.pixelformat, 4);
+        printf( "Selected Camera Mode:\n"
+                "  Width: %d\n"
+                "  Height: %d\n"
+                "  PixFmt: %s\n"
+                "  Field: %d\n",
+                fmt.fmt.pix.width,
+                fmt.fmt.pix.height,
+                fourcc,
+                fmt.fmt.pix.field);
+        return 0;
+}
+ 
+int init_mmap(int fd)
+{
+    struct v4l2_requestbuffers req = {0};
+    req.count = 1;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
+ 
+    if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req))
+    {
+        perror("Requesting Buffer");
+        return 1;
+    }
+ 
+    struct v4l2_buffer buf = {0};
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = 0;
+    if(-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf))
+    {
+        perror("Querying Buffer");
+        return 1;
+    }
+ 
+    buffer = mmap (NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+    printf("Length: %d\nAddress: %p\n", buf.length, buffer);
+    printf("Image Length: %d\n", buf.bytesused);
+ 
+    return 0;
 }
 
-void allocCamera(const char* file) 
+int capture_image(int fd)
 {
-    struct v4l2_capability cap;
-    struct v4l2_crop crop;
-    struct v4l2_format fmt;
+    struct v4l2_buffer buf = {0};
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = 0;
+    if(-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+    {
+        perror("Query Buffer");
+        return 1;
+    }
+ 
+    if(-1 == xioctl(fd, VIDIOC_STREAMON, &buf.type))
+    {
+        perror("Start Capture");
+        return 1;
+    }
+ 
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+    struct timeval tv = {0};
+    tv.tv_sec = 2;
+    int r = select(fd+1, &fds, NULL, NULL, &tv);
+    if(-1 == r)
+    {
+        perror("Waiting for Frame");
+        return 1;
+    }
+ 
+    if(-1 == xioctl(fd, VIDIOC_DQBUF, &buf))
+    {
+        perror("Retrieving Frame");
+        return 1;
+    }
+    printf ("saving image\n");
+}
 
-    int camera_fd = open(file, O_RDONLY);
+int open_device(const char* device_name) 
+{
+    return open(device_name, O_RDWR);
+}
 
-    if (-1 == xioctl (camera_fd, VIDIOC_QUERYCAP, &cap)) {
-        if (EINVAL == errno) {
-            fprintf (stderr, "%s is no V4L2 device\n", file);
-            exit (EXIT_FAILURE);
-        } else {
-            printf("\nError in ioctl VIDIOC_QUERYCAP\n\n");
-            exit(0);
+int close_device(int device) 
+{
+    close(device);
+}
+
+int main()
+{
+        int fd = open_device("/dev/video0");
+
+        if (fd == -1)
+        {
+                perror("Opening video device");
+                return 1;
         }
-    }
 
-    if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
-        fprintf (stderr, "%s is no video capture device\n",
-                file);
-        exit (EXIT_FAILURE);
-    }
+        if(print_caps(fd))
+            return 1;
+        
+        if(init_mmap(fd))
+            return 1;
 
-    if (!(cap.capabilities & V4L2_CAP_READWRITE)) {
-        fprintf (stderr, "%s does not support read i/o\n",
-                file);
-        exit (EXIT_FAILURE);
-    }
-
-    CLEAR(fmt);
-
-    fmt.type    = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    fmt.fmt.pix.width       = 640; 
-    fmt.fmt.pix.height      = 480;
-    fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
-    
-    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_RGB24;
-    //fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
-
-    fmt.fmt.pix.field       = V4L2_FIELD_INTERLACED;
-    if (-1 == xioctl(camera_fd, VIDIOC_S_FMT, &fmt)) {
-        printf("VIDIOC_S_FMT");
-    }
+        int i;
+        for(i=0; i<5; i++)
+        {
+            if(capture_image(fd))
+                return 1;
+        }
+        close_device(fd);
+        return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use std::ptr;
+mod v4l {
+      
+    #[link(name = "v4l-rust")]
+    extern {
+        fn open_device(device_name: &str) -> i64;
+    }
 
-mod rust_cam {
-
+    pub fn rust_open_device(device_name: &str) -> i64 {
+        unsafe {
+            open_device(device_name)
+        }
+    }
 }
 
 fn main() -> () {
-    
+    println!("{}", v4l::rust_open_device("/dev/video0"));
 }
